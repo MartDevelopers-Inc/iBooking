93ce4eba40c7368f854068bd38052df8939909a5
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-28 18:12:51 +0300

    Implement Logout

--- partials/main_menu.php
+++ partials/main_menu.php
@@ -7,92 +7,18 @@
         </div>
         <ul class="nav nav-pills flex-column ">
             <li class="nav-item">
-                <a class="nav-link active" href="index.html">Home
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
+                <a class="nav-link" href="home">Home
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="analytics.html">Analytics
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="nearby.html">Near by
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="requested.html">Request Sent
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="newoffer.html">New Offers
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="bookings.html">My Bookings
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="setting.html">Settings
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="pages.html">Pages
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="controls.html">Controls
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="colorscheme.html">Color Scheme
-                    <svg xmlns='http://www.w3.org/2000/svg' class="icon-size-16 arrow" viewBox='0 0 512 512'>
-                        <title>ionicons-v5-a</title>
-                        <polyline points='184 112 328 256 184 400' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px' />
-                    </svg>
-                </a>
-            </li>
+
         </ul>
-        <a href="signin.html" class="text-danger my-3 d-block">Sign out</a>
+        <a href="logout" class="text-danger my-3 d-block">Sign Out</a>
         <button class="btn btn-danger sqaure-btn close text-white"><svg xmlns='http://www.w3.org/2000/svg' class="icon-size-24" viewBox='0 0 512 512'>
                 <title>ionicons-v5-l</title>
                 <line x1='368' y1='368' x2='144' y2='144' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px' />
                 <line x1='368' y1='144' x2='144' y2='368' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px' />
-            </svg></button>
+            </svg>
+        </button>
     </div>
 
 </div>
