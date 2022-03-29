532eecd0201da6202e4d86ee5989b2fcb68a6388
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-29 20:09:51 +0300

    All Unit Tests Pass On Admin Module

--- views/home.php
+++ views/home.php
@@ -163,8 +163,8 @@ require_once('../partials/head.php');
                 <div class="swiper-wrapper">
                     <?php
                     $ret = "SELECT * FROM host_service hs
-                        INNER JOIN service_types st ON st.service_id = hs.host_service_service_id
-                        INNER JOIN host h ON h.host_id = hs.host_service_host_id";
+                    INNER JOIN service_types st ON st.service_id = hs.host_service_service_id
+                    INNER JOIN host h ON h.host_id = hs.host_service_host_id";
                     $stmt = $mysqli->prepare($ret);
                     $stmt->execute(); //ok
                     $res = $stmt->get_result();
