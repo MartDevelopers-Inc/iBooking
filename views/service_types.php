d305e9c775e0383e8d6afda1ffb4ac97d2802e1c
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-29 12:37:24 +0300

    Implement Service Types

--- views/service_types.php
+++ views/service_types.php
@@ -119,14 +119,14 @@ require_once('../partials/head.php');
         <hr>
         <div class="container mt-4 text-right">
             <button type="button" class="btn btn-primary m-2" data-toggle="modal" data-target="#add_modal">
-                Register Host
+                Register Service
             </button>
         </div>
         <div class="modal fade" id="add_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-sm modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenterTitle">Register Host</h5>
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Register Service</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -136,14 +136,14 @@ require_once('../partials/head.php');
                             <div class="row mt-">
                                 <div class="col-12 col-md-6">
                                     <div class="form-group floating-form-group">
-                                        <input type="text" name="host_name" class="form-control floating-input">
-                                        <label class="floating-label">Full Name</label>
+                                        <input type="text" name="service_number" value="<?php echo $a . $b; ?>" class="form-control floating-input">
+                                        <label class="floating-label">Service Number</label>
                                     </div>
                                 </div>
                                 <div class="col-12 col-md-6">
                                     <div class="form-group floating-form-group">
-                                        <input type="text" name="host_phone_no" class="form-control floating-input">
-                                        <label class="floating-label">Contacts</label>
+                                        <input type="text" name="service_name" class="form-control floating-input">
+                                        <label class="floating-label">Service Name</label>
                                     </div>
                                 </div>
                                 <div class="col-12 col-md-6">
@@ -154,19 +154,13 @@ require_once('../partials/head.php');
                                 </div>
                                 <div class="col-12 col-md-6">
                                     <div class="form-group floating-form-group">
-                                        <input type="text" name="host_address" class="form-control floating-input">
-                                        <label class="floating-label">Address</label>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <div class="form-group floating-form-group">
-                                        <input type="password" name="login_password" class="form-control floating-input">
-                                        <label class="floating-label">Password</label>
+                                        <textarea type="text" name="service_description" class="form-control floating-input"></textarea>
+                                        <label class="floating-label">Service Description</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-12 col-md-6 text-right">
-                                <input type="submit" name="add_host" value="Submit" class="btn btn-sm btn-primary">
+                                <input type="submit" name="add_service" value="Submit" class="btn btn-sm btn-primary">
                             </div>
                         </form>
                     </div>
@@ -180,10 +174,7 @@ require_once('../partials/head.php');
                     <div class="row">
                         <div class="col">
                             <h6 class="text-dark my-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
-                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
-                                </svg>
-                                <span class="text-center vm ml-2">Registered Hosts</span>
+                                <span class="text-center vm ml-2">Registered Services</span>
                             </h6>
                         </div>
                     </div>
@@ -191,11 +182,11 @@ require_once('../partials/head.php');
                 <div class="card-body">
                     <div class="row px-2">
                         <?php
-                        $ret = "SELECT * FROM host";
+                        $ret = "SELECT * FROM service_types";
                         $stmt = $mysqli->prepare($ret);
                         $stmt->execute(); //ok
                         $res = $stmt->get_result();
-                        while ($host = $res->fetch_object()) {
+                        while ($services = $res->fetch_object()) {
                         ?>
                             <div class="col-12 px-2">
                                 <div class="card card-round border border-success text-dark">
@@ -206,28 +197,24 @@ require_once('../partials/head.php');
                                                     <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
                                                     <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
                                                 </svg>
-                                                Name: <?php echo $host->host_name; ?> <br>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-paper" viewBox="0 0 16 16">
-                                                    <path d="M4 0a2 2 0 0 0-2 2v1.133l-.941.502A2 2 0 0 0 0 5.4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5.4a2 2 0 0 0-1.059-1.765L14 3.133V2a2 2 0 0 0-2-2H4Zm10 4.267.47.25A1 1 0 0 1 15 5.4v.817l-1 .6v-2.55Zm-1 3.15-3.75 2.25L8 8.917l-1.25.75L3 7.417V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v5.417Zm-11-.6-1-.6V5.4a1 1 0 0 1 .53-.882L2 4.267v2.55Zm13 .566v5.734l-4.778-2.867L15 7.383Zm-.035 6.88A1 1 0 0 1 14 15H2a1 1 0 0 1-.965-.738L8 10.083l6.965 4.18ZM1 13.116V7.383l4.778 2.867L1 13.117Z" />
+                                                Number: <?php echo $services->service_number; ?> <br>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+                                                    <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                                                    <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
                                                 </svg>
-                                                Email: <?php echo $host->host_email; ?> <br>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-                                                    <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-                                                </svg>
-                                                Mobile: <?php echo $host->host_phone_no; ?> <br>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-                                                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                                                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                                                </svg>
-                                                Address: <?php echo $host->host_address; ?>
+                                                Name: <?php echo $services->service_name; ?> <br>
+                                            </p>
+                                            <hr>
+                                            <p class="">
+                                                <?php echo $services->service_description; ?>
                                             </p>
                                         </div>
                                     </div>
                                     <div class="card-footer text-right mb-3 sm">
-                                        <a href="host?view=<?php echo $host->host_id; ?>" class="badge  badge-pill badge-warning">
+                                        <a href="service?view=<?php echo $services->service_id; ?>" class="badge  badge-pill badge-warning">
                                             Update
                                         </a>
-                                        <a href="hosts?delete=<?php echo $host->host_id; ?>" class="badge badge-pill badge-danger">
+                                        <a href="hosts?delete=<?php echo $services->service_id; ?>" class="badge badge-pill badge-danger">
                                             Delete
                                         </a>
                                     </div>
