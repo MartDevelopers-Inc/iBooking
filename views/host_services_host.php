1708632225a6d86311616c898f1135db7b4234da
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-30 08:06:03 +0300

    Implement Host Services

---
+++ views/host_services_host.php
@@ -0,0 +1,291 @@
+<?php
+/*
+ * martdevelopers254@gmail.com
+ *
+ *
+ * The MartDevelopers Inc End User License Agreement
+ *
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * 1. GRANT OF LICENSE
+ * MartDevelopers Inc hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ * install and activate this system on two separated computers solely for your personal and non-commercial use,
+ * unless you have purchased a commercial license from MartDevelopers Inc. Sharing this Software with other individuals, 
+ * or allowing other individuals to view the contents of this Software, is in violation of this license.
+ * You may not make the Software available on a network, or in any way provide the Software to multiple users
+ * unless you have first purchased at least a multi-user license from MartDevelopers Inc.
+ *
+ * 2. COPYRIGHT 
+ * The Software is owned by MartDevelopers Inc and protected by copyright law and international copyright treaties. 
+ * You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ * 3. RESTRICTIONS ON USE
+ * You may not, and you may not permit others to
+ * (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ * (b) modify, distribute, or create derivative works of the Software;
+ * (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ * otherwise exploit the Software.  
+ *
+ * 4. TERM
+ * This License is effective until terminated. 
+ * You may terminate it at any time by destroying the Software, together with all copies thereof.
+ * This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ * Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ * 5. NO OTHER WARRANTIES. 
+ * MartDevelopers Inc  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ * MartDevelopers Inc SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ * SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ * ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ * THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ * HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ * 6. SEVERABILITY
+ * In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ * affect the validity of the remaining portions of this license.
+ *
+ * 7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL MartDevelopers Inc  OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ * CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ * USE OF THE SOFTWARE, EVEN IF MartDevelopers Inc HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ * IN NO EVENT WILL MartDevelopers Inc  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ * TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ */
+session_start();
+require_once('../config/config.php');
+require_once('../config/codeGen.php');
+require_once('../config/checklogin.php');
+admin_check_login();
+
+/* Host Service */
+if (isset($_POST['add_service'])) {
+    $host_service_id = $sys_gen_id;
+    $host_service_host_id  = $_SESSION['login_host_id'];
+    $host_service_service_id = $_POST['host_service_service_id'];
+    $host_service_description = $_POST['host_service_description'];
+    $host_service_cost_description = $_POST['host_service_cost_description'];
+    $host_service_location = $_POST['host_service_location'];
+
+    /* Persist*/
+    $sql = "INSERT INTO host_service(host_service_id, host_service_host_id, host_service_service_id, host_service_description,
+    host_service_cost_description, host_service_location) VALUES(?,?,?,?,?,?)";
+    $prepare = $mysqli->prepare($sql);
+    $bind = $prepare->bind_param(
+        'ssssss',
+        $host_service_id,
+        $host_service_host_id,
+        $host_service_service_id,
+        $host_service_description,
+        $host_service_cost_description,
+        $host_service_location
+    );
+    $prepare->execute();
+    if ($prepare) {
+        $success = "Host Service Added";
+    } else {
+        $err = "Failed!, Please Try Again";
+    }
+}
+
+/* Delete Service */
+if (isset($_GET['delete'])) {
+    $delete = $_GET['delete'];
+    /* Persist */
+    $sql = "DELETE FROM host_service WHERE host_service_id = '$delete'";
+    $prepare = $mysqli->prepare($sql);
+    $prepare->execute();
+    if ($prepare) {
+        $_SESSION['success'] = "Host Service Deleted";
+        header('Location: host_services_host');
+        exit;
+    } else {
+        $err = "Failed!, Please Try Again";
+    }
+}
+require_once('../partials/head.php');
+?>
+
+<body class="body-scroll d-flex flex-column h-100 menu-overlay">
+
+    <!-- screen loader -->
+    <?php require_once('../partials/preloader.php'); ?>
+
+
+    <!-- menu main -->
+    <?php require_once('../partials/main_menu.php'); ?>
+    <!-- Begin page content -->
+    <div class="flex-shrink-0">
+        <!-- Fixed navbar -->
+        <?php require_once('../partials/header.php'); ?>
+        <br><br>
+        <hr>
+        <div class="container mt-4 text-right">
+            <button type="button" class="btn btn-primary m-2" data-toggle="modal" data-target="#add_modal">
+                Add Host Service
+            </button>
+        </div>
+        <div class="modal fade" id="add_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-sm modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Register Host Service</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form method="POST">
+                            <div class="row mt-">
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group floating-form-group">
+                                        <select type="text" name="host_service_service_id" class="form-control floating-input">
+                                            <?php
+                                            $ret = "SELECT * FROM service_types";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($service = $res->fetch_object()) {
+                                            ?>
+                                                <option value="<?php echo $service->service_id; ?>"><?php echo $service->service_number . ' - ' . $service->service_name; ?></option>
+                                            <?php } ?>
+                                        </select>
+                                        <label class="floating-label">Services</label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group floating-form-group">
+                                        <input type="text" name="host_service_location" required class="form-control floating-input">
+                                        <label class="floating-label">Location</label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group floating-form-group">
+                                        <input type="text" name="host_service_cost_description" required class="form-control floating-input">
+                                        <label class="floating-label">Cost Description</label>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group floating-form-group">
+                                        <textarea type="text" name="host_service_description" required class="form-control floating-input"></textarea>
+                                        <label class="floating-label">Service Description</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 text-right">
+                                <input type="submit" name="add_service" value="Submit" class="btn btn-sm btn-primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hosts -->
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col">
+                            <h6 class="text-dark my-1">
+                                <span class="text-center vm ml-2">Registered Host Services</span>
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row px-2">
+                        <?php
+                        $host_id = $_SESSION['login_host_id'];
+                        $ret = "SELECT * FROM host_service hs
+                        INNER JOIN service_types st ON st.service_id = hs.host_service_service_id
+                        INNER JOIN host h ON h.host_id = hs.host_service_host_id";
+                        $stmt = $mysqli->prepare($ret);
+                        $stmt->execute(); //ok
+                        $res = $stmt->get_result();
+                        while ($services = $res->fetch_object()) {
+                        ?>
+                            <div class="col-12 px-2">
+                                <div class="card card-round border border-success text-dark">
+                                    <div class="card-body d-flex align-items-center">
+                                        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                            <div class="carousel-inner">
+                                                <?php
+                                                /* Load Images */
+                                                $sql = "SELECT * FROM host_service_files 
+                                                WHERE file_host_service_id = '$services->host_service_id'
+                                                GROUP BY file_host_service_id ";
+                                                $stmt = $mysqli->prepare($sql);
+                                                $stmt->execute(); //ok
+                                                $imag = $stmt->get_result();
+                                                while ($services_images = $imag->fetch_object()) {
+                                                ?>
+                                                    <div class="carousel-item active">
+                                                        <img src="../public/services/<?php echo $services_images->file_data; ?> " class="d-block w-100" alt="">
+                                                    </div>
+                                                <?php } ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="card-content">
+                                            <p class="mb-3 sm">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16">
+                                                    <path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0z" />
+                                                    <path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1zm0 5.586 7 7L13.586 9l-7-7H2v4.586z" />
+                                                </svg>
+                                                Number: <?php echo $services->service_number; ?> <br>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-bounding-box" viewBox="0 0 16 16">
+                                                    <path d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z" />
+                                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                                </svg>
+                                                Host Name: <?php echo $services->host_name; ?> <br>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+                                                    <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                                                </svg>
+                                                Contacts: <?php echo $services->host_phone_no; ?> <br>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                                </svg>
+                                                Location: <?php echo $services->host_service_location; ?> <br>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z" />
+                                                    <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z" />
+                                                    <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z" />
+                                                    <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z" />
+                                                </svg>
+                                                Cost : <?php echo $services->host_service_cost_description; ?> <br>
+                                            </p>
+                                            <hr>
+                                            <p class="">
+                                                <?php echo $services->host_service_description; ?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer text-center mb-3 sm">
+                                        <a href="host_service_host_file?view=<?php echo $services->host_service_id; ?>" class="badge  badge-pill badge-primary">
+                                            Add File
+                                        </a>
+                                        <a href="host_service_host?view=<?php echo $services->host_service_id; ?>" class="badge  badge-pill badge-warning">
+                                            Update
+                                        </a>
+                                        <a href="host_services_host?delete=<?php echo $services->host_service_id; ?>" class="badge badge-pill badge-danger">
+                                            Delete
+                                        </a>
+                                    </div>
+                                </div>
+                                <br>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br><br>
+    </div>
+    <?php require_once('../partials/scripts.php'); ?>
+</body>
+
+</html>
