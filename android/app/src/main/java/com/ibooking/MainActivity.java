d17776dfcb684aefb9d19e459714dc5ad1b079cd
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-30 17:18:02 +0300

    Splash Screen Design

--- android/app/src/main/java/com/ibooking/MainActivity.java
+++ android/app/src/main/java/com/ibooking/MainActivity.java
@@ -2,13 +2,40 @@ package com.ibooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView logo;
+    private static int splashTimeOut = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
         setContentView(R.layout.activity_main);
+        logo = (ImageView) findViewById(R.id.);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, WebViewActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, splashTimeOut);
+
+        Animation my_anim = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+        logo.startAnimation(my_anim);
     }
 }
