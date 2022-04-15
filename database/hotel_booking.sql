de0aae0b37e8690d83323ee98fbe68518033802f
Martin Mbithi <martdevelopers254@gmail.com>
2022-04-15 11:24:07 +0300

    Attach Updated Database Dump Files

--- database/hotel_booking.sql
+++ database/hotel_booking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 30, 2022 at 08:34 AM
+-- Generation Time: Apr 15, 2022 at 10:21 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -67,8 +67,8 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`booking_id`, `booking_user_id`, `booking_ref`, `booking_description`, `booking_host_service_id`, `booking_date`, `booking_time`, `booking_requested_start_date`, `booking_requested_start_time`, `booking_requested_end_date`, `booking_requested_end_time`, `booking_status`) VALUES
-('fb37cc5456deb65ea4f7cda3b19507406d33be8bcca1', '25e2030e22cad437d4019c9023a8974f7178905e85ed', 'URXLK46103', 'I need to book your condo for a couple of days.', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3b', '2022-03-30', '09:00', '2022-04-08', '20:00', '2022-04-06', '12:00', 'Approved'),
-('fbd885861bdcda0bfa7251c82bb18d0da946f8a97952', 'efa27769c933a6c073c48d91207a464c7cd0e9c49005', 'YHUVN12586', 'I need to book your condo for a couple of days.', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3b', '2022-03-30', '12:00', '2022-04-01', '21:00', '2022-04-09', '08:00', 'Approved');
+('835bedd652aef1cf63bb29c6ede782d1cf09d85ccaf1', '25e2030e22cad437d4019c9023a8974f7178905e85ed', 'IVSWO10256', 'I want to reserve this beach house for a week. ', 'd8b5425cfe4890e48bca362b5505f65c14ee8b78b62c', '2022-04-15', '11:00', '2022-04-22', '22:00', '2022-04-29', '00:00', 'Approved'),
+('d728ac80df56fa0b523c64a3c0aa7b0b35aeeaafb2d2', 'efa27769c933a6c073c48d91207a464c7cd0e9c49005', 'XSHVD29346', 'Would like to rent this house for couple of days when i am out of town', 'd8b5425cfe4890e48bca362b5505f65c14ee8b78b62c', '2022-04-15', '09:00', '2022-04-16', '21:00', '2022-04-22', '21:00', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -91,7 +91,8 @@ CREATE TABLE `host` (
 INSERT INTO `host` (`host_id`, `host_name`, `host_phone_no`, `host_email`, `host_address`) VALUES
 ('2258ed024562b73df5bb7264e736c16c10e985c4a997', 'Janet Jackson Doe', '+12-9009843-09', 'jjd@m.com', '123 Demo'),
 ('5a404e1a06d97f6e55e85b4c9a2bce2252c92d1ee209', 'Thomas Jacksons', '+12-9313009843', 'thomasj@m.com', '123 Demo'),
-('5b964674c221d8d24204b5e88a1358e06fc10ce2bf22', 'Jane Doe', '+12-9009843', 'janedoe@m.com', '123 Demo');
+('5b964674c221d8d24204b5e88a1358e06fc10ce2bf22', 'Jane Doe', '+12-9009843', 'janedoe@m.com', '123 Demo'),
+('6864b7c2504d4e69253662e3e1ee60e6037dcb3b62bd', 'Chemutai Alaska', '+12-9313009843', 'chemutaialaska@gmail.com', 'Nairobi 9021');
 
 -- --------------------------------------------------------
 
@@ -113,10 +114,8 @@ CREATE TABLE `host_service` (
 --
 
 INSERT INTO `host_service` (`host_service_id`, `host_service_host_id`, `host_service_service_id`, `host_service_description`, `host_service_cost_description`, `host_service_location`) VALUES
-('0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3b', '5a404e1a06d97f6e55e85b4c9a2bce2252c92d1ee209', '718e2e3a95671833832a4838170e5481022db39e4e21', 'Clean condo, free wifi, running water, serene environment overlooking the city', 'Ksh 1900 Per Night', '9027 Lakeview'),
-('0fad87034f200957eeeb3f9fd908331970a4826c51d2', '5a404e1a06d97f6e55e85b4c9a2bce2252c92d1ee209', '718e2e3a95671833832a4838170e5481022db39e4e21', 'Exquisite condos over the hills', 'Ksh1500 Per Night', '89-Strt Chicago'),
-('48db6e0e1b4f03515720ca02326a8607134057a73f6d', '5a404e1a06d97f6e55e85b4c9a2bce2252c92d1ee209', '9999132413fsdvas ', 'Exquisite, single room hotel wiith a nice view of the city', 'Ksh 500 Per Night', '127 Lakeview'),
-('dedb251273c29eca10fa378c52615b890905384e2191', '5b964674c221d8d24204b5e88a1358e06fc10ce2bf22', '718e2e3a95671833832a4838170e5481022db39e4e21', 'A palace in the hills overlooking the city for rent out.', 'Ksh 1400 Per Night', '90 Milwaukee');
+('d8b5425cfe4890e48bca362b5505f65c14ee8b78b62c', '2258ed024562b73df5bb7264e736c16c10e985c4a997', 'cf6c662409b707929dacafcaab9f9620b54828047aa6', 'Best beach houses in Mombasa', 'Ksh 4500 Per Night', 'Mtwapa, Mombasa'),
+('daf50589268556e52a5e4f9ff7cb7c99cbe24970bd90', '2258ed024562b73df5bb7264e736c16c10e985c4a997', '3d8c550b2b1830a17b3b9f2b7e596cfd4943f8db7f32', 'Fully furnished cabins in the city', 'Ksh 1500 Per Night', 'Westlands, Nairobi');
 
 -- --------------------------------------------------------
 
@@ -136,13 +135,9 @@ CREATE TABLE `host_service_files` (
 --
 
 INSERT INTO `host_service_files` (`file_id`, `file_type`, `file_data`, `file_host_service_id`) VALUES
-('1694e480208fe7d2289364dc1cd1a17e3bbdbf659f2f', 'Images', 'image-4.jpg', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3b'),
-('5a4059c6a729edb3a9ac775f8fe47a4e91ccc6082007', 'Image', '48db6e0e1b4f03515720ca02326a8607134057a73f6dimage-5.jpg', '48db6e0e1b4f03515720ca02326a8607134057a73f6d'),
-('821d1ec25ad111a4b65dafa201aa47154cb02cf89c47', 'Image', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3bimage-3.jpg', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3b'),
-('9c91f1bd849d014f528b48925756fcb035aab15874c1', 'Image', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3bimage-2.jpg', '0754a1fbcf3006851d3716f9ea033bfb89ad989c3b3b'),
-('9df14fe79f6a36d26373d6b931a65658502d850f09f1', 'Image', 'dedb251273c29eca10fa378c52615b890905384e2191image-6.jpg', 'dedb251273c29eca10fa378c52615b890905384e2191'),
-('aaf28af41200693c6644de62f7ed8c8167842f7c98e6', 'Image', 'dedb251273c29eca10fa378c52615b890905384e2191image-5.jpg', 'dedb251273c29eca10fa378c52615b890905384e2191'),
-('ba7bbe2c1334444219da552a51638dfd814ccc99c595', 'Image', '0fad87034f200957eeeb3f9fd908331970a4826c51d2image-4.jpg', '0fad87034f200957eeeb3f9fd908331970a4826c51d2');
+('0ddd6721f826ed5db6a1e8b5b8bd927d85c9715616f8', 'Image', 'd8b5425cfe4890e48bca362b5505f65c14ee8b78b62cimage-4.jpg', 'd8b5425cfe4890e48bca362b5505f65c14ee8b78b62c'),
+('34293f66dffe0d3a6858ed0ffefbba769d0608f4cc85', 'Image', 'daf50589268556e52a5e4f9ff7cb7c99cbe24970bd90image-6.jpg', 'daf50589268556e52a5e4f9ff7cb7c99cbe24970bd90'),
+('d15cc52f0f34588a0efc18fb6f9693422da94a053e72', 'Image', 'd8b5425cfe4890e48bca362b5505f65c14ee8b78b62cimage-3.jpg', 'd8b5425cfe4890e48bca362b5505f65c14ee8b78b62c');
 
 -- --------------------------------------------------------
 
@@ -167,11 +162,12 @@ CREATE TABLE `login` (
 INSERT INTO `login` (`login_id`, `login_email`, `login_password`, `login_rank`, `login_user_id`, `login_admin_id`, `login_host_id`) VALUES
 ('0f299f6401509092fe3e3100dd47a4045a50338d244a', 'ab90@gmail.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'User', '3095d236a6a559becb32cf25ff0e86c97ebf2e173447', NULL, NULL),
 ('30ca17717a3c0e8f013f6424072466bddedaaefc7585', 'thomasj@m.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'Host', NULL, NULL, '5a404e1a06d97f6e55e85b4c9a2bce2252c92d1ee209'),
+('342c53e940b5158506c4b2fa287195716adf399883d8', 'chemutaialaska@gmail.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'Host', NULL, NULL, '6864b7c2504d4e69253662e3e1ee60e6037dcb3b62bd'),
 ('3922d2808dfd38feb8bf12e3a71e696ff09146c6f132', 'janedoe@m.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'Host', NULL, NULL, '2258ed024562b73df5bb7264e736c16c10e985c4a997'),
 ('8d525a405c8d0be9522fd4a7c3967504da2b3dcfb476', 'janedoe@m.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'Host', NULL, NULL, '5b964674c221d8d24204b5e88a1358e06fc10ce2bf22'),
 ('944ae7e3550a49181b329220360debc0d989b9b9d892', 'janet@gmail.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'User', 'efa27769c933a6c073c48d91207a464c7cd0e9c49005', NULL, NULL),
 ('a69681bcf334ae1305fd3c994f5683f', 'sysadmin@gmail.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'Administrator', NULL, 'efa27769c933a6c073c48d91207a46', NULL),
-('bbdd68dff770c22b8dab0ba0829286ec9c0017ba7d6e', 'alexisbd12@gmail.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'User', '25e2030e22cad437d4019c9023a8974f7178905e85ed', NULL, NULL);
+('bbdd68dff770c22b8dab0ba0829286ec9c0017ba7d6e', 'mart@gmail.com', 'a69681bcf334ae130217fea4505fd3c994f5683f', 'User', '25e2030e22cad437d4019c9023a8974f7178905e85ed', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -192,8 +188,7 @@ CREATE TABLE `payment` (
 --
 
 INSERT INTO `payment` (`payment_id`, `payment_amount`, `payment_date`, `payment_mode`, `payment_booking_id`) VALUES
-('0911b99d7ca74f9f53ec51106d1819f243ea54527f52', '55000', '2022-03-29', 'Debit / Credit Card', 'fbd885861bdcda0bfa7251c82bb18d0da946f8a97952'),
-('d10ca36d27d858644e900adc948138952ebe0d3b4e56', '10000', '2022-03-30', 'Cash', 'fb37cc5456deb65ea4f7cda3b19507406d33be8bcca1');
+('2c935de16bfe355899d85148baa14902de1afac3cafc', '45000', '2022-04-22', 'Mpesa', '835bedd652aef1cf63bb29c6ede782d1cf09d85ccaf1');
 
 -- --------------------------------------------------------
 
@@ -213,9 +208,11 @@ CREATE TABLE `service_types` (
 --
 
 INSERT INTO `service_types` (`service_id`, `service_name`, `service_number`, `service_description`) VALUES
-('718e2e3a95671833832a4838170e5481022db39e4e21', 'Condo Hosting', 'ZDBFN15307', 'I have a condo up in the hills, which i need to host a user for a pocket friendly price.'),
-('87654567845678', 'Hosting ', '001', 'Curious which components explicitly require our JavaScript and Popper? Click the show components link below. If you’re at all unsure about the general page structure, keep reading for an example page template.'),
-('9999132413fsdvas ', 'Renting ', '002', 'Curious which components explicitly require our JavaScript and Popper? Click the show components link below. If you’re at all unsure about the general page structure, keep reading for an example page template.');
+('111192719ba13c435615051b62dc0a6adc1a8072dd49', 'Rental', 'XZAKE47165', 'Unfurnished rental houses'),
+('3d8c550b2b1830a17b3b9f2b7e596cfd4943f8db7f32', 'Cabins', 'GHQEO50978', 'Have a wild taste on your houses, this one is for you'),
+('5cb23eb8d90f16e53250b093d67509816b95d77e2848', 'Condo', 'XTQRG16487', 'Fully furnished, clean condo'),
+('6f904f63aeb511b44875355c0f9ce9e06575f6788977', 'Appartments', 'ZDOVQ04718', 'Semi furnished appartments'),
+('cf6c662409b707929dacafcaab9f9620b54828047aa6', 'Beach House', 'HEWZR93241', 'Fully furnished beach house');
 
 -- --------------------------------------------------------
 
@@ -235,7 +232,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_mobile`, `user_email`) VALUES
-('25e2030e22cad437d4019c9023a8974f7178905e85ed', 'Alexis Baldwin', '+25412345689', 'alexis12@gmail.com'),
+('25e2030e22cad437d4019c9023a8974f7178905e85ed', 'Mart', '+25412345689', 'mart@gmail.com'),
 ('3095d236a6a559becb32cf25ff0e86c97ebf2e173447', 'Baldwin Alec', '+00987654335', 'ab90@gmail.com'),
 ('efa27769c933a6c073c48d91207a464c7cd0e9c49005', 'Janet D Monroe', '+900-099423', 'janet@gmail.com');
 
